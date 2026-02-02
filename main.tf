754745f6998acb1d6032b2bb7d566d923fd18922
Aniketh <anikethani9997@gmail.com>
2026-02-02 20:06:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "local_file" "my_pet" {
-  filename = "pet.txt"
-  content = "MISSION COMPLETED "
+resource "local_file" "my_world" {
+  filename = var.filename
+  content  = var.content
+  }
+resource "random_pet" "petname" {
+  prefix    = var.prefix
+  separator = "."
+  length    = var.length
 }
