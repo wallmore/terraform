321780f74ee87b8a9e8d1a1f271970674174fbad
Aniketh <anikethani9997@gmail.com>
2026-02-05 16:06:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,34 @@
-resource "local_file" "my_world" {
-  filename = "sonata.txt"
-  content  = "my petname is ${random_pet.petname.id}"
+resource "aws_iam_user" "Admin-user" {
+  name = "Einstein"
+  tags = {
+    description = "technical team lead"
   }
-resource "random_pet" "petname" {
-  prefix    = "MR"
-  separator = "."
-  length    = "1"
 }
-output "petname" {
-  value = random_pet.petname.id
+resource "aws_iam_policy" "admin-user" {
+  name   = "adminuser"
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "1234567890",
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+resource "aws_iam_user_policy_attachment" "Einstein-adminuser-access" {
+  user = aws_iam_user.Admin-user.name
+  policy_arn = aws_iam_policy.admin-user.arn
+}
+resource "aws_iam_policy" "ec2_access" {
+  name   = "ec2_access"
+  policy = file("ec2-policy.json")
+}
+resource "aws_iam_user_policy_attachment" "Einstein-ec2-access" {
+  user = aws_iam_user.Admin-user.name
+  policy_arn = aws_iam_policy.ec2_access.arn
 }
